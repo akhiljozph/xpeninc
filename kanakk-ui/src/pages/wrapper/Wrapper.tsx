6c20610f6d90a44d4d9b52d4cf9c5f0f7c30bf96
akhiljozph <ajpulluvelil@gmail.com>
2026-03-06 23:18:00 +0530

    KANK-23 - Added a common page in ui app.

---
+++ kanakk-ui/src/pages/wrapper/Wrapper.tsx
@@ -0,0 +1,12 @@
+import type { WrapperProps } from "./Wrapper.types";
+
+import "./Wrapper.scss";
+
+const Wrapper = (props: WrapperProps) => {
+    return (
+        <>
+            wrapper works!
+        </>
+    )
+}
+export default Wrapper;
